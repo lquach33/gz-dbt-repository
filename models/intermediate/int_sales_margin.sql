3afb2727e0b67b108a0e42e62f49c2a8ed2f34d6
laurent quach <laurent.quach@gmail.com>
2024-05-08 14:37:41 +0000

    margin percent

--- models/intermediate/int_sales_margin.sql
+++ models/intermediate/int_sales_margin.sql
@@ -1,4 +1,4 @@
-select product.products_id as products_id
+with sales_margin as (select product.products_id as products_id
     ,product.purchase_price as price
     ,sales.date_date as date_date
     ,sales.orders_id as orders_id
@@ -9,4 +9,7 @@ select product.products_id as products_id
 from {{ref('stg_raw__product')}} as product
 join 
     {{ref('stg_raw__sales')}} as sales
-using (products_id)
+using (products_id))
+select*,
+{{ margin_percent('revenue','margin') }} as margin_pourcentage
+from sales_margin
